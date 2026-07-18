4156768f2392de83c13b3fa17c97cb1b9f6d89c7
inoodex0 <sneara@inoodex.com>
2026-07-18 12:50:56 +0600

    Comment out ShurjopayServiceProvider in app.php to disable Shurjopay integration

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        shurjopayv2\ShurjopayLaravelPackage8\ShurjopayServiceProvider::class
+        // shurjopayv2\ShurjopayLaravelPackage8\ShurjopayServiceProvider::class
 
     ],
 
